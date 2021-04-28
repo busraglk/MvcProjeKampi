2fab5d4ba7ebd837741b30cdc1cfc26c9c351bc1
busragelik@gmail.com <busragelik@gmail.com>
2021-04-28 19:14:00 +0300

    .

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,18 @@ namespace MvcProjeKampi.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+
+        CategoryManager categoryManager = new CategoryManager();
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = categoryManager.GetAllBL();
+            return View(categoryvalues);
+        }
+
     }
 }
